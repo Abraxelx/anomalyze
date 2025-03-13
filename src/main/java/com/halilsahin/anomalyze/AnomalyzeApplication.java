f28fcbb41402e8c889dcdcdf1b7120ad39d59a9f
Halil Sahin <xelilim@hotmail.com>
2025-03-13 14:44:56 +0300

    initial commit

---
+++ src/main/java/com/halilsahin/anomalyze/AnomalyzeApplication.java
@@ -0,0 +1,13 @@
+package com.halilsahin.anomalyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomalyzeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnomalyzeApplication.class, args);
+    }
+
+}
